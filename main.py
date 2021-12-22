e5d2e26f81549c29fe6e7a32cf85e40540090e62
Eduardo Marcelo <duduprogrammer@gmail.com>
2021-12-22 20:18:43 -0300

    Criando mensagem de aviso

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 num = 1
 contagem = 1
 
+print('Aviso: Caro usuário, você poderá verificar até 3 números, caso queira verificar mais alguns deverá rodar o código novamente')
 
 while num != 0 and contagem <= 3:
     num = int(input('Digite um número: '))
